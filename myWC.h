d7d5dd4aecce64b93edf3a8d81caca64f22fee8f
SirMichael <michaelcmcmaseko@gmail.com>
2024-02-19 22:57:01 +0200

    textfile added and header file complete

---
+++ myWC.h
@@ -0,0 +1,16 @@
+#ifndef MYWC_H
+#define MYWC_H
+
+#include <string>
+#include <vector>
+
+namespace MSKMIC017 {
+    struct CharInfo {
+        char character;
+        long count;
+    };
+
+    void countCharsWordsLinesAndPrint();
+}
+
+#endif
