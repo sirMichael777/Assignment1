0db33c6345d498c7439414fb985a5533ab0d4aa4
SirMichael <michaelcmcmaseko@gmail.com>
2024-02-28 17:31:56 +0200

    Code modified to print a sorted frequency table

--- myWC.cpp
+++ myWC.cpp
@@ -1,17 +1,24 @@
 #include "myWC.h"
 #include <iostream>
 #include <vector>
-#include <cctype>
 #include <sstream>
 #include <algorithm>
 
 namespace MSKMIC017 {
+
+    // Function to check if a character is alphanumeric
+    bool myIsAlnum(char c) {
+        return (c >= 'a' && c <= 'z') ||
+               (c >= 'A' && c <= 'Z') ||
+               (c >= '0' && c <= '9');
+    }
+
     void countCharsWordsLinesAndPrint() {
         std::string line;
         long lineCount = 0;
         long wordCount = 0;
-        long charCount = 0;
-        std::vector<CharInfo> charFreq;
+        long characterCount = 0;
+        std::vector<CharInfo> characterFrequency;
 
         while (std::getline(std::cin, line)) {
             ++lineCount;
@@ -19,43 +26,40 @@ namespace MSKMIC017 {
 
             std::istringstream iss(line);
             std::string word;
+            bool lineHasWord = false;
             while (iss >> word) {
                 bool hasWordAlphanumeric = false;
-                if (!word.empty()) { // Check if the word is not empty
+                if (!word.empty()) {
                     ++wordCount;
+                    lineHasWord = true;
                     for (char c : word) {
-                        if (std::isalnum(c)) {
-                            ++charCount;
+                        if (myIsAlnum(c)) { // Using custom function myIsAlnum instead of std::isalnum
+                            ++characterCount;
                             char ch = std::tolower(c);
-                            auto it = std::find_if(charFreq.begin(), charFreq.end(), [ch](const CharInfo& info) {
+                            auto it = std::find_if(characterFrequency.begin(), characterFrequency.end(), [ch](const CharInfo& info) {
                                 return info.character == ch;
                             });
-                            if (it != charFreq.end()) {
+                            if (it != characterFrequency.end()) {
                                 ++it->count;
                             } else {
-                                charFreq.push_back({ch, 1});
+                                characterFrequency.push_back({ch, 1});
                             }
                             hasWordAlphanumeric = true;
                         }
                     }
                 }
+                
                 hasAlphanumeric |= hasWordAlphanumeric;
-            }
-            if (!hasAlphanumeric) {
-                --wordCount;
             }
         }
 
-        // Sort the results alphabetically
-        std::sort(charFreq.begin(), charFreq.end(), [](const CharInfo& a, const CharInfo& b) {
+        std::sort(characterFrequency.begin(), characterFrequency.end(), [](const CharInfo& a, const CharInfo& b) {
             return a.character < b.character;
         });
 
-        // prints out the number of lines, words and characters
-        std::cout << lineCount << " " << wordCount << " " << charCount << " [";
+        std::cout << lineCount << " " << wordCount << " " << characterCount << " [";
 
-        // Print the character frequency table to the console
-        for (const CharInfo& info : charFreq) {
+        for (const CharInfo& info : characterFrequency) {
             std::cout << info.character << ":" << info.count << " ";
         }
 
